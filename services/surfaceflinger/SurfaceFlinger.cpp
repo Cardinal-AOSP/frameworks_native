d5a8e6dc3c75e9729a0857a81b7dc7596a4bba28
Iris Chang <iris.chang@mediatek.com>
2018-07-01 23:29:11 +0800

    DO NOT MERGE: Fix landscape LCM issue
    When device uses landscape LCM, the nature of the screen shows that
    the device is a portrait device. In this case, we find GPU filter
    function is opened by mistake when capturing screen and CTS case
    fails because image pixel value is changed.
    
    The solution is to correct the GPU filter by hworientation.
    
    Bug: 69691076
    Test:
    1. Capture screen
    2. android.uirendering.cts.testclasses.SurfaceViewTests#testMovingWhiteSurfaceView
    
    Change-Id: I67b75fc8fab188d24f1d5febff20d9bd10c15204

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4448,8 +4448,14 @@ void SurfaceFlinger::renderScreenImplLocked(
         sourceCrop = tr.transform(sourceCrop);
     }
 
-    const bool filtering = static_cast<int32_t>(reqWidth) != sourceCrop.width() ||
-            static_cast<int32_t>(reqHeight) != sourceCrop.height();
+    bool filtering = false;
+    if (mHwOrientation & DisplayState::eOrientationSwapMask) {
+        filtering = static_cast<int32_t>(reqWidth) != sourceCrop.width() ||
+                static_cast<int32_t>(reqHeight) != sourceCrop.height();
+    } else {
+        filtering = static_cast<int32_t>(reqWidth) != sourceCrop.width() ||
+                static_cast<int32_t>(reqHeight) != sourceCrop.height();
+    }
 
     // ensure that sourceCrop is inside screen
     if (sourceCrop.left < 0) {
